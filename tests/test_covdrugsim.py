9e0997444d6d9bf0bb59c06e1f7947b07fe42b9f
Jon-Ting <jonting97@gmail.com>
2023-07-05 18:01:09 +1000

    test: updated tests/ directory

--- tests/test_covdrugsim.py
+++ tests/test_covdrugsim.py
@@ -1,11 +1,25 @@
-import pytest
+from os.path import exists, isdir, isfile
+from shutil import rmtree
+
+import numpy as np
+from pytest import approx, fixture, mark
+
 from covdrugsim.qmcalc.unitConv.unitConv import E_unit_conv
 
 
-# TODO: More tests to be developed
-
-
 def test_E_unit_conv():
-    assert E_unit_conv(100, None) == pytest.approx((100, 418.40000000000003))
+    assert E_unit_conv(100, None) == approx((100, 418.40000000000003))
 
+
+#def test_vis():
+#    assert exists('tests/outputs/betaCarbonCharges.png'), 'betaCarbonCharges.png not found'
+#    assert isfile('tests/outputs/betaCarbonCharges.png'), 'betaCarbonCharges.png is not a file'
+#    assert exists('tests/outputs/distortionInteractionAnalysis.png'), 'distortionInteractionAnalysis.png not found'
+#    assert isfile('tests/outputs/distortionInteractionAnalysis.png'), 'distortionInteractionAnalysis.png is not a file'
+#    assert exists('test/outputs/ligandLUMOenergy.png'), 'ligandLUMOenergy.png not found'
+#    assert isfile('tests/outputs/ligandLUMOenergy.png'), 'ligandLUMOenergy.png is not a file'
+#    assert exists('test/outputs/example.pdf'), 'example.pdf not found'
+#    assert isfile('tests/outputs/example.pdf'), 'example.pdf is not a file'
+#    if isdir('tests/outputs'):
+#        rmtree('tests/outputs')
 
