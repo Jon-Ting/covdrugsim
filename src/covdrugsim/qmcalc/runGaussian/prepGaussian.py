0661eb63b9b1d22007003ec711d0be99482580b1
Jon-Ting <jonting97@gmail.com>
2023-07-07 14:57:38 +1000

    docs(prepGaussian): added docstrings to functions in prepGaussian.py

--- src/covdrugsim/qmcalc/runGaussian/prepGaussian.py
+++ src/covdrugsim/qmcalc/runGaussian/prepGaussian.py
@@ -4,58 +4,169 @@ from os.path import isdir
 from covdrugsim.qmcalc.runGaussian.settings import keywordDict
 
 
-def genFromDir(inputDir, WALLTIME, VMEM, MEM, JOBFS, CHARGE, SPIN, METHOD, BASIS_SET, SOLVENT, SOLVENT_MODEL, CLUSTER, CALC_TYPE, NCPUS, SOFTWARE, VERSION):
-    """Generate Gaussian input job files and submission files for molecules in given directories"""
-    assert CALC_TYPE in keywordDict.keys(), 'Calculation type not known!'
-    conformers = [g for g in os.listdir(inputDir) if isdir('{0}/{1}'.format(inputDir, g))]
-    for j, title in enumerate(conformers):
-        conformer_dir = '{0}/{1}'.format(inputDir, title)
-        combination = '{0}/{1}'.format(METHOD, BASIS_SET)
-        SCRF = ' scrf=({0},solvent={1})'.format(SOLVENT, SOLVENT_MODEL)
-        gaussInpGeomOpt(title, conformer_dir, MEM, NCPUS, combination, keywordDict[CALC_TYPE]['freq'],
-                           SCRF, keywordDict[CALC_TYPE]['type'], CHARGE, SPIN)
-        writeJobScript(title, conformer_dir, CLUSTER, NCPUS, SOFTWARE, WALLTIME, VMEM, JOBFS, VERSION)
+def genFromDir(inputDir, 
+               method='B3LYP', basisSet='6-31+G(d)', solvent='water', solventModel='smd',
+               mem=8000, ncpus=8, calcType='GOVF', charge=0, spin=1, 
+               scheduler='pbs', cluster='gadi', 
+               walltime='10:00:00', vmem=8000, jobfs=2400, software='g16', version='c01'):
+    """
+    Generate Gaussian input job files and submission files for molecules under all directories under a specified directory ('inputDir').
+
+    Parameters
+    ----------
+    inputDir : str
+        Directory path to the input directories.
+    method : str
+        Keyword for DFT method specification in Gaussian.
+    basisSet : str
+        Keyword for basis set specification in Gaussian.
+    solvent : str
+        Keyword for solvent specification in Gaussian.
+    solventModel : str
+        Keyword for SCRF method specification in Gaussian.
+    mem : Union[int,str]
+        Amount of memory to request for the Gaussian job.
+    ncpus : Union[int,str]
+        Number of CPUs to request for the job.
+    calcType : str
+        Type of calculation (e.g. 'GOVF' for normal geometry optimisation; 'TSGOVF' for transition state geometry optimisation, 
+        'SPEiS' for single point energy calculation, refer to 'keywordDict' for other options).
+    charge : int
+        Charge of the molecule (pay special attention if you have a transition state).
+    spin : int
+        Spin of the molecule.
+    scheduler : str
+        Scheduler to submit the job to.
+    cluster : {'gadi', 'uq-rcc'}
+        Cluster to run the job on.
+    walltime : str
+        Wall time to request for the job.
+    vmem : Union[int,str]
+        Amount of memory to request for the HPC job.
+    jobfs : Union[int,str]
+        Amount of Jobfs memory to request for the job.
+    software : str
+        Software to use for the job.
+    version : str
+        Version of the software.
+    """
+    assert calcType in keywordDict.keys(), 'Calculation type not known!'
+    conformers = [g for g in os.listdir(inputDir) if isdir(f"{inputDir}/{g}"))]
+    for name in conformers:
+        conformerDir = f"{inputDir}/{name}"
+        combination = f"{method}/{basisSet}"
+        scrf = f" scrf=({solvent},solvent={solventModel})"
+        gaussInpGeomOpt(name, conformerDir, 
+                        mem, ncpus, combination, keywordDict[calcType]['freq'], 
+                        scrf, keywordDict[calcType]['type'], charge, spin)
+        writeJobScript(name, conformerDir, 
+                       scheduler, cluster, 
+                       ncpus, walltime, vmem, jobfs, software, version)
 
 
-def gaussInpGeomOpt(title, dirc, mem, ncpus, combination, freq, scrf, calc_type, charge, spin):
-    with open('{0}/{1}.inp'.format(dirc, title), 'w') as f:
-        f.write('%mem={0}mb\n%nproc={1}\n%chk={2}.chk\n'.format(mem, ncpus, title))
-        f.write('# {0}{1}{2}{3}'.format(combination, scrf, calc_type, freq))
-        f.write('\n\n{0}\n\n{1} {2}\n'.format(title, charge, spin))
-        with open('{0}/{1}.xyz'.format(dirc, title), 'r') as g:
-            lineList = g.readlines()
-            hasEnergyName = 'Energy' in lineList[1] or 'TI' in lineList[1] or 'Name' in lineList[1]  # Potential bug-breeding spots
+def gaussInpGeomOpt(name, dirPath, 
+                    mem=8000, ncpus=8, 
+                    combination='B3LYP/6-31+G(d)', scrf=' scrf=(water,solvent=smd)', freq=' freq=noraman', 
+                    calcType='GOVF', charge=0, spin=1):
+    """
+    Generate a Gaussian input file based on specified inputs.
+
+    Parameters
+    ----------
+    name : str
+        Name of the jobscript, also used for input and output file of the software to run.
+    dirPath : str
+        Directory path to store the jobscript.
+    mem : Union[int,str]
+        Amount of memory to request for the Gaussian job.
+    ncpus : Union[int,str]
+        Number of CPUs to request for the job.
+    combination : str
+        Keyword for DFT method and basis set specification in Gaussian.
+    scrf : str
+        Keyword for solvent specification in Gaussian.
+    freq : str
+        Keyword for force constant and resulting vibrational frequencies computation in Gaussian.
+    calcType : str
+        Type of calculation (e.g. 'GOVF' for normal geometry optimisation; 'TSGOVF' for transition state geometry optimisation, 
+        'SPEiS' for single point energy calculation, refer to 'keywordDict' for other options).
+    charge : int
+        Charge of the molecule (pay special attention if you have a transition state).
+    spin : int
+        Spin of the molecule.
+    """
+    with open(f"{dirPath}/{name}.inp", 'w') as inpFile:
+        inpFile.write(f"%mem={mem}mb\n%nproc={ncpus}\n%chk={name}.chk\n"
+        inpFile.write(f"# {combination}{scrf}{calcType}{freq}"
+        inpFile.write(f"\n\n{name}\n\n{charge} {spin}\n"
+        with open(f"{dirPath}/{name}.xyz", 'r') as xyzFile:
+            lineList = xyzFile.readlines()
+            hasEnergyName = 'Energy' in lineList[1] or 'TI' in lineList[1] or 'Name' in lineList[1]  # Potential bugs
             hasPath = ':' in lineList[1]
-            has_blank = '\n' in lineList[1]
-            for i, line in enumerate(lineList):
-                if hasEnergyName and i > 1:
-                    f.write('{0}'.format(line))
-                elif hasPath and i > 1:
-                    f.write('\n{0}'.format(line)) if i == 2 else f.write('{0}'.format(line))
-                elif has_blank and i > 1:
-                    f.write('{0}'.format(line))
-                elif not(hasEnergyName) and not(hasPath) and not(has_blank) and i > 0:
-                    f.write('{0}'.format(line))
-                else:
-                    pass
-            f.write('\n')  # Necessary
+            hasBlank = '\n' in lineList[1]
+            for (i, line) in enumerate(lineList):
+                if hasEnergyName and (i > 1):
+                    inpFile.write(line)
+                elif hasPath and (i > 1):
+                    inpFile.write(f"\n{line}" if i == 2 else inpFile.write(line)
+                elif hasBlank and (i > 1):
+                    inpFile.write(line)
+                elif not(hasEnergyName) and not(hasPath) and not(has_blank) and (i > 0):
+                    inpFile.write(line)
+            inpFile.write('\n')
 
 
-def writeJobScript(title, dirc, cluster, ncpus, software, walltime, vmem, jobfs, version):
-    with open('{0}/{1}.sh'.format(dirc, title), 'w') as f:
-        if cluster == 'Raijin':
-            f.write('#!/bin/bash\n#PBS -l wd\n#PBS -q normal\n')
-            f.write('#PBS -l walltime={0},mem={1}mb,ncpus={2},software={3},jobfs={4}mb'.format(walltime, vmem, ncpus, software, jobfs))
-            f.write('\n\nmodule load gaussian/{0}{1}'.format(software, version))
-            f.write('\n{0} < {1}.inp > {1}.out 2>&1'.format(software, title))
-        elif cluster == 'RCC':
-            f.write('#!/bin/bash\n#PBS -S /bin/bash\n#PBS -l walltime={0}\n#PBS -A UQ-SCI-SCMB\n'.format(walltime))
-            f.write('#PBS -l select=1:ncpus={0}:mem={1}MB'.format(ncpus, vmem))
-            f.write('\n\ncd $PBS_O_WORKDIR')
-            f.write('\n\nmodule load gaussian/{0}-{1}-bash'.format(software, version.upper()))
-            f.write('\n{0} < {1}.inp > {1}.out'.format(software, title))
-        else:
-            raise Exception('Cluster not recognized!')
+def writeJobScript(name, dirPath='.', 
+                   scheduler='pbs', cluster='gadi', 
+                   ncpus=8, walltime='10:00:00', vmem=8000, jobfs=2400, software='g16', version='c01'):
+    """
+    Generate a HPC jobscript based on specified inputs. 
+
+    Parameters
+    ----------
+    name : str
+        Name of the jobscript, also used for input and output file of the software to run.
+    dirPath : str
+        Directory path to store the jobscript.
+    scheduler : str
+        Scheduler to submit the job to.
+    cluster : {'gadi', 'uq-rcc'}
+        Cluster to run the job on.
+    ncpus : Union[int,str]
+        Number of CPUs to request for the job.
+    walltime : str
+        Wall time to request for the job.
+    vmem : Union[int,str]
+        Amount of memory to request for the HPC job.
+    jobfs : Union[int,str]
+        Amount of Jobfs memory to request for the job.
+    software : str
+        Software to use for the job.
+    version : str
+        Version of the software.
+    
+    Notes
+    -----
+    - If your HPC system does not use PBS jobscript modifications will be needed for the function.
+    - Feel free to change the default values according to your most commonly used settings.
+    """
+    with open(f"{dirPath}/{name}.sh", 'w') as f:
+        if scheduler == 'pbs':
+            if cluster == 'gadi':
+                f.write('#!/bin/bash\n#PBS -l wd\n#PBS -q normal\n')
+                f.write(f"#PBS -l walltime={walltime},mem={vmem}mb,ncpus={ncpus},software={software},jobfs={jobfs}mb")
+                f.write(f"\n\nmodule load gaussian/{software}{version}")
+                f.write(f"\n{software} < {name}.inp > {name}.out 2>&1")
+            elif cluster == 'uq-rcc':
+                f.write(f"#!/bin/bash\n#PBS -S /bin/bash\n#PBS -l walltime={walltime}\n#PBS -A UQ-SCI-SCMB\n")
+                f.write(f"#PBS -l select=1:ncpus={ncpus}:mem={vmem}MB")
+                f.write('\n\ncd $PBS_O_WORKDIR')
+                f.write(f"\n\nmodule load gaussian/{software}-{version.upper()}-bash")
+                f.write(f"\n{software} < {name}.inp > {name}.out")
+            else:
+                raise Exception(f"Cluster {cluster} not recognised/accommodated for yet!")
+        else: 
+            raise Exception(f"Scheduler {scheduler} not recognised/accommodated for yet!")
 
 
 if __name__ == "__main__":
@@ -63,7 +174,7 @@ if __name__ == "__main__":
     jobList = ['geom_opt', 'spe', 'SCS']
 
     # Default values, unlikely changing
-    NCPUS, SOFTWARE, VERSION = int(8), 'g16', 'b01'
+    NCPUS, SOFTWARE, VERSION = int(8), 'g16', 'c01'
 
     # Change these!
     job = jobList[0]
@@ -74,8 +185,5 @@ if __name__ == "__main__":
     SOLVENT, SOLVENT_MODEL = "water", "smd"
     CLUSTER = "Raijin"
     CALC_TYPE = 'GOVF'
-    # "GOVF" if normal geometry optimisation,
-    # "TSGOVF" if transition state geometry optimisation,
-    # "SPEiS" if single point energy calculation
 
     genFromDir(inputDir, WALLTIME, VMEM, MEM, JOBFS, CHARGE, SPIN, METHOD, BASIS_SET, SOLVENT, SOLVENT_MODEL, CLUSTER, CALC_TYPE, NCPUS, SOFTWARE, VERSION)
