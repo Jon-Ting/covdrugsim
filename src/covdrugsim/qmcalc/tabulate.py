8b61893690e1dc57b807594199d7a2ad2c0f0bbe
Jon-Ting <jonting97@gmail.com>
2023-07-08 13:05:28 +1000

    refactor(tabulate): renamed variables and functions in tabulate.py

--- src/covdrugsim/qmcalc/tabulate.py
+++ src/covdrugsim/qmcalc/tabulate.py
@@ -5,26 +5,26 @@ import re
 import pandas as pd
 
 
-def sortHuman(alist):
-    """Sort a given alist in a more 'human' way"""
-    convert = lambda text: float(text) if text.isdigit() else text
-    alphanum = lambda key: [convert(c) for c in re.split('([-+]?[0-9]*\.?[0-9]*)', key)]
-    alist.sort(key=alphanum)
-    return alist
+def sortNatural(targetList):
+    """Sort a given list in a more natural way."""
+    tryConvertNumeric = lambda text: float(text) if text.isdigit() else text
+    alphanum = lambda key: [tryConvertNumeric(c) for c in re.split('([-+]?[0-9]*\.?[0-9]*)', key)]
+    targetList.sort(key=alphanum)
+    return targetList
 
 
-def replaceMultiple(mainStr, toBeReplaced, newStr):
-    """Replace multiple strings of mainStr"""
-    for elem in toBeReplaced:
-        if elem in mainStr:
-            mainStr = mainStr.replace(elem, newStr)
-    return mainStr
+def replaceMultiple(str1, strsToReplace, str2):
+    """Replace multiple strings of str1 by str2."""
+    for strToReplace in strsToReplace:
+        if strToReplace in str1:
+            str1 = str1.replace(strToReplace, str2)
+    return str1
 
 
 def findVal(lineList, targetStr):
-    """Find the values of interest from Gaussian output files"""
+    """Find the values of interest from Gaussian output files."""
     val, isEnergy, isMethod = None, 'Energies' in targetStr[0] or 'Enthalpies' in targetStr[0], '%chk' in targetStr[0]
-    for i, string in enumerate(targetStr):
+    for string in targetStr:
         for j, line in enumerate(lineList):
             if string in line:
                 if isMethod:
@@ -51,37 +51,45 @@ def findVal(lineList, targetStr):
     return val
 
 
-def writeToExcel(dir_path):
-    """Write a new Excel file tabulating the quantities of interest"""
-    methodList, titleList, moleculeList, conformerList, NImagList, ZList, EList, HList, GList, MP2List = [], [], [], [], [], [], [], [], [], []
+def writeToExcel(inputDirPath, verbose=False):
+    """
+    Tabulate the quantities of interest to an Excel document.
+    """
+    groups = [f for f in listdir(inputDirPath) if isdir('{0}/{1}'.format(inputDirPath, f))]
+
+    if verbose:
+        print("\n# Tabulating values of interest from Gaussian .out files to an Excel sheet...")
+        print("\n# Input directory:\n", inputDirPath, "\n\n# Groups:\n", groups, "\n")
+
+    methodList, nameList, moleculeList, moleculeList, NImagList, ZList, EList, HList, GList, MP2List = [], [], [], [], [], [], [], [], [], []
     varFillList = [methodList, NImagList, ZList, EList, HList, GList]
     keywordList = [['%chk'], ['NI', 'Im'], ['zero-point Energies'], ['HF'], ['thermal Enthalpies'], ['thermal Free Energies']]
-    for j, title in enumerate(groups):
-        conformerDir = '{0}/{1}'.format(inputDir, title)
-        print("Conformer:", title)
+    for name in groups:
+        moleculeDir = '{0}/{1}'.format(inputDirPath, name)
+        print("Molecule:", name)
         try:
-            with open('{0}/{1}.out'.format(conformerDir, title), 'r') as f:
+            with open('{0}/{1}.out'.format(moleculeDir, name), 'r') as f:
                 lineList = f.readlines()
                 lineList.reverse()
                 for i, varList in enumerate(varFillList):
                     val = findVal(lineList, keywordList[i])
                     varList.append(val)
-                titleList.append(title)
-                moleculeList.append(replace_multiple(title.split('c')[0].replace('_', ''), ['TR', 'TSS', 'TP'], ''))
-                conformerList.append('c' + title.split('c')[-1])
+                nameList.append(name)
+                moleculeList.append(replaceMultiple(name.split('c')[0].replace('_', ''), ['TR', 'TSS', 'TP'], ''))
+                moleculeList.append('c' + name.split('c')[-1])
         except FileNotFoundError:
-            print("{0}/{1}.out not found!".format(conformerDir, title))
+            print("{0}/{1}.out not found!".format(moleculeDir, name))
             continue
-    data = {'Method': methodList, 'Title': titleList, 'Molecule': moleculeList, 'Conformer': conformerList, 'NImag': NImagList,
+    data = {'Method': methodList, 'Name': nameList, 'Molecule': moleculeList, 'Molecule': moleculeList, 'NImag': NImagList,
            'Z (Hartree)': ZList, 'E (Hartree)': EList, 'H (Hartree)': HList, 'G (Hartree)': GList}
-    df = pd.DataFrame(data, columns=['Method', 'Title', 'Molecule', 'Conformer', 'NImag', 'Z (Hartree)', 'E (Hartree)', 'H (Hartree)', 'G (Hartree)'])
-    titleList = sortHuman(titleList)
-    sortedDF = df.set_index('Title').reindex(titleList).reset_index()
+    df = pd.DataFrame(data, columns=['Method', 'Name', 'Molecule', 'Molecule', 'NImag', 'Z (Hartree)', 'E (Hartree)', 'H (Hartree)', 'G (Hartree)'])
+    nameList = sortNatural(nameList)
+    sortedDF = df.set_index('Name').reindex(nameList).reset_index()
     print("# Sorted data frame:\n", sortedDF)
 
     print("# Writing to Excel sheet...")
-    # sortedDF.to_excel('{0}/Energies.xlsx'.format(inputDir), sheet_name='Sheet1')
-    writer = pd.ExcelWriter('{0}/Energies.xlsx'.format(inputDir), engine='xlsxwriter')
+    # sortedDF.to_excel('{0}/Energies.xlsx'.format(inputDirPath), sheet_name='Sheet1')
+    writer = pd.ExcelWriter('{0}/Energies.xlsx'.format(inputDirPath), engine='xlsxwriter')
     sortedDF.to_excel(writer, startrow=1, sheet_name='Sheet1', index=False)
     workbook = writer.book
     worksheet = writer.sheets['Sheet1']
@@ -94,12 +102,6 @@ def writeToExcel(dir_path):
 
 
 if __name__ == "__main__":
-
-    # Change this!
-    inputDir = "/User/kahochow/Desktop/Li_mechanism/Li_work/Reactant"
-
-    groups = [f for f in listdir(inputDir) if isdir('{0}/{1}'.format(inputDir, f))]
-    print("\n# Tabulating values of interest from Gaussian .out files to an Excel sheet...")
-    print("\n# Input directory:\n", inputDir, "\n\n# Groups:\n", groups, "\n")
-    workbook = writeToExcel(inputDir)
+    inputDirPath = '/mnt/c/Users/ASUS/Documents/covdrugsim/src/covdrugsim/data/exampleXYZs'  # To be modified!
+    workbook = writeToExcel(inputDirPath, verbose=True)
 
