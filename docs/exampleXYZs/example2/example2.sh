17c9eefa9255a5569dd356e0daeb160381fa078f
Jon-Ting <jonting97@gmail.com>
2023-07-08 19:37:05 +1000

    docs(example): updated examples

---
+++ docs/exampleXYZs/example2/example2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#PBS -l wd
+#PBS -q normal
+#PBS -l walltime=24:00:00,mem=8000mb,ncpus=8,software=g16,jobfs=9000mb,storage=scratch/p39
+
+module load gaussian/g16c01
+g16 < example2.inp > example2.out 2>&1
